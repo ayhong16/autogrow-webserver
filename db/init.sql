7570a6013c818936887ce05945eca46659b68b7a
nrgbistro <nolangelinas@gmail.com>
2024-03-31 20:05:52 -0400

    Initial commit

---
+++ db/init.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE environment;
+
+-- Connect to the new database
+\c environment;
+
+-- Create a table in the new database
+CREATE TABLE data (
+    id SERIAL PRIMARY KEY,
+    time TIMESTAMP,
+    temp FLOAT,
+    humd FLOAT,
+    ph FLOAT,
+);
