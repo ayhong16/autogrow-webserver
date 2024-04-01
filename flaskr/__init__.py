1124bc74be218428281921bb46104b8f7bde15fe
Ubuntu <ubuntu@ip-172-31-93-64.ec2.internal>
2024-04-01 00:09:25 +0000

    set up flask

---
+++ flaskr/__init__.py
@@ -0,0 +1,62 @@
+from flask import Flask, request, jsonify
+import psycopg2
+from datetime import datetime
+import os
+import csv
+
+def get_db_connection():
+    return psycopg2.connect(
+        dbname="environment",
+        user="postgres",
+        password="password",
+        host="localhost",
+        port="5432"
+    )
+
+def insert_sensor_data(temperature, humidity, timestamp):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO environment_data (temp, humd, timestamp) VALUES (%s, %s, %s)", (temperature, humidity, timestamp))
+    conn.commit()
+    conn.close()
+
+def psql_to_csv():
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM environment_data")
+    rows = cursor.fetchall()
+    col_names = [desc[0] for desc in cursor.description]
+    with open("./environment_data.csv", 'w', newline='') as csv_file:
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(col_names)
+    csv_writer.writerows(rows)
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        # a default secret that should be overridden by instance config
+        SECRET_KEY="dev",
+        # store the database in the instance folder
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
+    )
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # load the test config if passed in
+        app.config.update(test_config)
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/environmental_data', methods=['POST'])
+    def receive_sensor_data():
+        data = request.get_json()
+        temperature = round(data.get('temp'), 2)
+        humidity = data.get('humd')
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        insert_sensor_data(temperature, humidity, timestamp)
+        return jsonify({'message': 'Sensor data received and storedsuccessfully.'})
+    return app
