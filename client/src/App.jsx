a1a526cb1d4704f11b890fef15f7817ff276dead
Ashley Hong <ayhong8@gmail.com>
2024-04-03 01:18:03 -0400

    connect react server to flask server

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,21 @@
-import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [array, setArray] = useState([]);
 
   const fetchAPI = async () => {
-    const response = await axios.get("http://127.0.0.1:5000/api/data")
-    console.log(response.data)
-  }
+    const response = await axios.get("/api/data");
+    console.log(response.data);
+  };
+
+  useEffect(() => {
+    fetchAPI()
+  }, []);
 
   return (
     <>
