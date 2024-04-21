dc6d7efeb7090359ed6cd4c2496a3b5701a50760
nrgbistro <nolangelinas@gmail.com>
2024-04-21 18:36:24 -0400

    Fix proxying

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,27 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from 'dotenv';
 
-export default ({ mode }) => {
-  dotenv.config({ path: `./.env.${mode}` });
-  // now you can access config with process.env.{configName}
 
-  const apiURL =  process.env.VITE_API_URL;
-
-  console.log('API URL:', apiURL);
-
-  return defineConfig({
-    plugins: [react()],
-    server: {
-        host: true,
-        port : 3000,
-        proxy: {
-          '/api': {
-              target: apiURL,
-              changeOrigin: true,
-              secure: false,
-          }
+export default defineConfig({
+  plugins: [react()],
+  server: {
+      host: true,
+      port : 3000,
+      proxy: {
+        '/api': {
+            target: 'http://127.0.0.1:5000',
+            changeOrigin: true,
+            secure: false,
         }
-    },
-  })
-}
+      }
+  },
+})
